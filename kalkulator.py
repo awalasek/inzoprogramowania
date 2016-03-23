80bb1b29560b2edfab5a55850aaf600111273734
Aleksandra Walasek <37204@student.ue.poznan.pl>
2016-03-23 12:57:11 +0100

    dodalam funkcje get_info()

--- kalkulator.py
+++ kalkulator.py
@@ -2,6 +2,9 @@ def dodawanie():
 	a = int(input("Podaj a "))
 	a = int(input("Podaj b "))
 	print(a+b)
+def get_info():
+	return "To jest program kalkulator stworzony przeze mnie i nikogo innego"
 	
+print(get_info())	
 dodawanie()
 input()
