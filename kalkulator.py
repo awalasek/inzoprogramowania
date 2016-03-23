b7ae5a13734dfd0e9de9fe7f97188fb52aa980ab
Aleksandra Walasek <37204@student.ue.poznan.pl>
2016-03-23 13:09:03 +0100

    Wprowadzono poprawki

--- kalkulator.py
+++ kalkulator.py
@@ -1,8 +1,9 @@
 def dodawanie(a,b):
 	return a + b
-	
-a=int(input("Podaj pierwsza liczbe "))	
-b=int(input("Podaj druga liczbe "))
-print(dodawanie(a,b))
-
+try:	
+	a=int(input("Podaj pierwsza liczbe "))	
+	b=int(input("Podaj druga liczbe "))
+	print(dodawanie(a,b))
+except ValueError as ve:
+	print('wprowadzono bledne dane, koniec')
 input()
